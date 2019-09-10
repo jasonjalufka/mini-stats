6e17145b83ed5068b3d789d621923ef37e8b1579
Jason Jalufka <jasonjalufka@gmail.com>
2019-09-10 11:05:34 -0700

    Add weather conditions to weather endpoint response

--- endpoints/weather.js
+++ endpoints/weather.js
@@ -2,14 +2,13 @@ const { getJSONResponse } = require('../utils');
 
 const weather = async function() {
   const data = await getJSONResponse(
-    `https://api.openweathermap.org/data/2.5/weather?lat=45.516647&lon=-122.6462477&units=imperial&appid=${
-      process.env.WEATHER_API_KEY
-    }`
+    `https://api.openweathermap.org/data/2.5/weather?lat=45.516647&lon=-122.6462477&units=imperial&appid=${process.env.WEATHER_API_KEY}`
   );
 
   return {
     temp: `${data.main.temp}°`,
-    city: data.name
+    city: data.name,
+    condition: data.weather[0].main
   };
 };
 
